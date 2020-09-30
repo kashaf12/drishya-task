41a356cb3a7b1479f9fbee9e8e45091f6648f653
kashaf12 <kashafaahmed@gmail.com>
2020-09-30 18:30:26 +0000

    Deploying to gh-pages from  @ 602b04599645407ace17c0ab89501e2de539f6e5 🚀

---
+++ precache-manifest.dff9c1e3abdbd60b26a7b8207b949d55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7210216dfd39f011b28b1fb5acfa5ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c627b5d1ae996776cef",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "34862a3d62711b6522d5",
+    "url": "/static/js/2.78564ccf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.78564ccf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c627b5d1ae996776cef",
+    "url": "/static/js/main.4b198f7e.chunk.js"
+  },
+  {
+    "revision": "b9b0af042051f705f003",
+    "url": "/static/js/runtime-main.69bb1756.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
