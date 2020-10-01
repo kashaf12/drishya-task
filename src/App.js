944417b6f1e71e789f30ebb8d5e1eefbec3b3cec
kashaf12 <kashafaahmed@gmail.com>
2020-10-01 16:53:25 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 /** @format */
 
+import { Typography } from "@material-ui/core";
+/** @format */
+
 import React from "react";
 import Header from "./Components/Header";
 
 function App() {
 	return (
 		<div className='App'>
-			<Header/>
+			<Header />
+			<Typography>Kashaf</Typography>
 		</div>
 	);
 }
